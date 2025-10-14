ed481675803189081a70fdbf9f7c487aff4eea76
enzokrebs8 <enzokrebs8@gmail.com>
2025-10-14 04:37:33 -0300

    Edit Allow Origin

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 // Cabeçalhos CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+// Cabeçalhos CORS
+
+$allowed_origins = [
+    'https://www.clidefit.com.br',
+    'https://clidefit.com.br',
+    'http://localhost:5173',
+];
+
+$http_origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($http_origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $http_origin);
+} else {
+    header("Access-Control-Allow-Origin: https://www.clidefit.com.br");
+}
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
